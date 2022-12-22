cf9af28a0461be2f69676e741e9b2386cb16a430
Худышка К <skinny.developer@gmail.com>
2022-12-23 00:35:40 +0300

    изображение с правой стороны

--- English/Word/WordCell.js
+++ English/Word/WordCell.js
@@ -1,8 +1,16 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function WordCell() {
+  const favoritImage = require('../assets/favorit/favorit.png')
+  const notFavoritImage = require('../assets/favorit/not_favorit.png')
+
   return (
     <View style={styles.testStyle}>
+      <Image
+        style={styles.imageStyle}
+        source={notFavoritImage}
+      />
       <Text style={styles.russLabel} > Текст на русском </Text>
       <Text style={styles.englLabel} > Текст на английском </Text>
     </View>
@@ -10,29 +18,27 @@ export default function WordCell() {
 }
 
 const styles = StyleSheet.create({
-    testStyle: {
-        backgroundColor: '#aaa',
-        height: 75
-    },
-    russLabel: {
-        top: 8,
-        paddingHorizontal: 16,
-        textAlign: 'left',
-        // height: 25,
-        fontSize: 25,
-        fontWeight: 'bold'
-      },
-    englLabel: {
-      top: 8,
-      paddingHorizontal: 16,
-      textAlign: 'left',
-      fontSize: 25
-    },
-    buttonStyle: {
-      height: 50,
-      width: 50,
-      top: 0,
-      right:0,
-      backgroundColor: '#888'
-    }
+  testStyle: {
+    backgroundColor: '#444',
+    height: 75
+  },
+  russLabel: {
+    top: 8,
+    paddingHorizontal: 16,
+    textAlign: 'left',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  englLabel: {
+    top: 8,
+    paddingHorizontal: 16,
+    textAlign: 'left',
+    fontSize: 25
+  },
+  imageStyle: {
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    right: 0
+  }
 });
